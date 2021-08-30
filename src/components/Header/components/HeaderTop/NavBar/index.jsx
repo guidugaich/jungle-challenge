4ff753cbf8773aa58eaca7b7c6c9268637e6933b
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-08-29 22:38:51 -0300

    nav bar component

---
+++ src/components/Header/components/HeaderTop/NavBar/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './style.css'
+
+export default function NavBar() {
+  return (
+    <nav>
+      <a>Create Your Nanny Share</a>
+      <a>Browse Shares</a>
+      <a>Our Story</a>
+    </nav>
+  )
+}
