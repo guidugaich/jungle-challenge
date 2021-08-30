c16add85e9cac59c79ca1a8590a71bf4eef82251
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-08-30 13:16:17 -0300

    header top style

--- src/components/Header/components/HeaderTop/index.jsx
+++ src/components/Header/components/HeaderTop/index.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+
 import Logo from './Logo/';
 import NavBar from './NavBar/';
-import SignUpBtn from './SignUpBtn';
-import SignInBtn from './SignInBtn';
+import SignUpBtn from './SignUpBtn/';
+import SignInBtn from './SignInBtn/';
 
 import './style.css';
 
 export default function index() {
   return (
-    <div classname="header-top-container">
+    <div className="header-top-container">
       <div className="top-left">
         <Logo />
         <NavBar />
