b318d2f36addf44cc3fc899556b28de1b7c4134a
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-08-30 13:15:58 -0300

    reorganizing folder structure

--- src/components/Header/components/HeaderTop/SignInBtn.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function SignInBtn() {
-  return (
-    <div>
-      Sign In Button
-    </div>
-  )
-}
--- src/components/Header/components/HeaderTop/SignUpBtn.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function SignUpBtn() {
-  return (
-    <div>
-      SignUp Button
-    </div>
-  )
-}
