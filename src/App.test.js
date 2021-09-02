6eed46c3304d8b93400efaf641b55a534b02b610
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-09-02 13:19:15 -0300

    adding one more test case

--- src/App.test.js
+++ src/App.test.js
@@ -5,10 +5,13 @@ import App from './App';
 const API_URL = 'https://api.jungledevs.com/api/v1/challenge-newsletter/';
 
 
-describe('Tests for the Nanny share landing page', () => {
-  test('User can input values to Hero section form and submit', () => {
+describe('Tests for the Hero section form', () => {
+  test('Submit button is not enabled if email format is not correct', () => {
     const { getByTestId } = render(<App />)
-    jest.spyOn(global, "fetch")
+
+    const submitBtn = getByTestId("submit-btn")
+    expect(submitBtn).toBeInTheDocument()
+    expect(submitBtn).not.toBeEnabled();
 
     const nameInput = getByTestId('name-input')
     expect(nameInput).toBeInTheDocument()
@@ -16,10 +19,26 @@ describe('Tests for the Nanny share landing page', () => {
 
     const emailInput = getByTestId('email-input')
     expect(emailInput).toBeInTheDocument()
+    userEvent.type(emailInput, 'johnny')
+
+    expect(submitBtn).not.toBeEnabled();
+
+    userEvent.type(emailInput, 'johnny@email.com')
+    
+    expect(submitBtn).toBeEnabled();
+  });
+
+  test('User can input values to Hero section form and submit', () => {
+    const { getByTestId } = render(<App />)
+    jest.spyOn(global, "fetch")
+
+    const nameInput = getByTestId('name-input')
+    userEvent.type(nameInput, 'johnny')
+
+    const emailInput = getByTestId('email-input')
     userEvent.type(emailInput, 'johnny@email.com')
 
     const submitBtn = getByTestId("submit-btn")
-    expect(submitBtn).toBeInTheDocument()
     userEvent.click(submitBtn)
     
     expect(global.fetch).toHaveBeenCalledWith(
